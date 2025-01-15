3b7efb2af35ff36ea0013f668e150f3c450f458e
huynhtri123 <huynhmtri1211@gmail.com>
2025-01-15 09:50:24 +0700

    add BE folder

---
+++ agro-server/src/main/java/org/agromarket/agro_server/AgroServerApplication.java
@@ -0,0 +1,13 @@
+package org.agromarket.agro_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroServerApplication.class, args);
+	}
+
+}
